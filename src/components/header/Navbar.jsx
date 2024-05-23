0d3ca46b7c564fc649d0e56ea851eca83cbaf41b
jdporr <jdporrazzo@gmail.com>
2024-05-23 13:51:36 -0400

    add comments to navbar.jsx

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -8,11 +8,14 @@ function Navbar() {
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-sunset text-4xl md:text-6xl font-captain font-bold">John Porrazzo's Portfolio</div>
         
+        {/* Burger Menu for Mobile View */}
         <div className="md:hidden">
           <label htmlFor="menu-toggle" className="cursor-pointer">
             <FaBars className="text-4xl text-ocean" />
           </label>
+           {/* Hidden Checkbox that Controls the Menu Toggle */}
           <input type="checkbox" id="menu-toggle" className="hidden peer" />
+          {/* Menu Items: Initially Hidden, Shown When Checkbox is Checked */}
           <div className="hidden peer-checked:flex flex-col space-y-2 absolute bg-kelp p-4 right-0 mt-4 font-captain text-4xl">
             <Link to="/" className="text-ocean hover:text-coral transition-colors duration-300">Home</Link>
             <Link to="/services" className="text-ocean hover:text-coral transition-colors duration-300">Services</Link>
@@ -21,6 +24,7 @@ function Navbar() {
           </div>
         </div>
 
+        {/* Links for Desktop View: Always Visible */}
         <div className="hidden md:flex space-x-4 text-4xl font-captain font-bold">
           <Link to="/" className="text-ocean hover:text-coral transition-colors duration-300">Home</Link>
           <Link to="/services" className="text-ocean hover:text-coral transition-colors duration-300">Services</Link>
