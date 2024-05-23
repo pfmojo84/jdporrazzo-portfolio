3a70954cbb2e716fc4150b4267e15a81379fad54
jdporr <jdporrazzo@gmail.com>
2024-05-23 17:04:43 -0400

    redirects cont.

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ function App() {
         <Route path='/services' element={ <Services />} />
         <Route path='/portfolio' element={ <Portfolio />} />
         <Route path='/contact' element={ <Contact />} />
+        <Route path='*' element={ <Home />} />
         </Routes>
 
         <Footer />
