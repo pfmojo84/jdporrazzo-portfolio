59a2a69f5c0b0070fa335570482a1409a43e1188
jdporr <jdporrazzo@gmail.com>
2024-06-24 11:33:25 -0400

    update labels in footer to comply with accessibility criteria

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,13 +7,13 @@ function Footer() {
   return (
     <footer className="bg-kelp text-white py-4 text-5xl flex flex-col items-center space-y-2">
       <div className='flex space-x-6'>
-        <a href="https://github.com/pfmojo84" target="_blank" rel="noopener noreferrer">
+        <a href="https://github.com/pfmojo84" target="_blank" rel="noopener noreferrer" aria-label='Github Profile'>
         <FaGithub />
         </a>
-        <a href="https://www.linkedin.com/in/john-porrazzo/" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/in/john-porrazzo/" target="_blank" rel="noopener noreferrer" aria-label='LinkedIn Profile'>
         <LuLinkedin />
         </a>
-        <a href="https://www.codecademy.com/profiles/johnPorrazzo_dev" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.codecademy.com/profiles/johnPorrazzo_dev" target="_blank" rel="noopener noreferrer" aria-label='CodeCademy Profile'>
         <SiCodecademy />
         </a>
       </div>
